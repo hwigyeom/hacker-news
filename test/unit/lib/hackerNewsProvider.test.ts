20082965eb2c85b260b03efec02c5fa0d647b8aa
Hwigyeom Noh (노휘겸) <whistle96@gmail.com>
2023-03-21 23:46:50 +0900

    feat: setup test environments (#1)
    jest 기반 테스트 환경 설정

---
+++ test/unit/lib/hackerNewsProvider.test.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import searchHackerNews, { BASE_URL, HackerNewsSearchResult } from '@src/lib/hackerNewsProvider';
+import { NodeJSQueriedResults } from './resources/hackerNewsResources';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+describe('Call HackerNews search api', () => {
+  it('should return normal results for node.js query.', async () => {
+    // given
+
+    // axios 호출에 대한 mock-up을 만든다.
+    const mockData: HackerNewsSearchResult = NodeJSQueriedResults;
+    mockedAxios.get.mockResolvedValueOnce({ data: mockData });
+
+    // when
+    const result = await searchHackerNews('node.js');
+
+    // then
+    expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}?query=node.js&tags=story&hitsPerPage=90`);
+    expect(result).toEqual(mockData);
+  });
+
+  it('should throw error when axios call fails.', async () => {
+    // given
+    mockedAxios.get.mockRejectedValueOnce(new Error('Network Error'));
+
+    // when
+    try {
+      await searchHackerNews('error');
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+      expect((err as Error).message).toEqual('Network Error');
+    }
+  });
+});
