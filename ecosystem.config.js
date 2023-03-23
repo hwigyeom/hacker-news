a66cad5654cc8393573cd060ebad831ab5108ff1
노휘겸 <whistle@douzone.com>
2023-03-23 17:54:37 +0900

    chore: setup PM2 process manager
    - PM2 적용
    - Typescript 트랜스파일 문제점 해결(`tsc-alias`)
    
    - pino를 이용한 로깅이 PM2 cluster mode에서는 동작하지 않을 확인
    
    Relates: #11

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hacker-news',
+      script: './dist/server.js',
+      max_memory_restart: '1G',
+      cron_restart: '0 */24 * * *',
+      watch: ['views', 'dist'],
+      ignore_watch: ['node_modules', 'public'],
+      watch_delay: 1000,
+      exp_backoff_restart_delay: 100, // 100ms
+      exec_mode: 'cluster',
+      instances: -1,
+    },
+  ],
+};
